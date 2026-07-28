da1f34de11388afbe5bf77229b37479a2d692eea
george1-lang <197446062+george1-lang@users.noreply.github.com>
2026-07-28 23:00:01 +0000

    feat(api): add spring boot entrypoint

---
+++ services/api/src/main/java/com/bookero/BookeroApplication.java
@@ -0,0 +1,12 @@
+package com.bookero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookeroApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BookeroApplication.class, args);
+  }
+}

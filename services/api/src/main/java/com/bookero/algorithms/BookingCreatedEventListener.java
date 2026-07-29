d7a3ba011ace34f309d1b37b6189930407626f26
george1-lang <197446062+george1-lang@users.noreply.github.com>
2026-07-29 03:03:34 +0000

    perf(api): run post-booking reprice off the request thread

--- services/api/src/main/java/com/bookero/algorithms/BookingCreatedEventListener.java
+++ services/api/src/main/java/com/bookero/algorithms/BookingCreatedEventListener.java
@@ -4,6 +4,7 @@ import com.bookero.common.BookingCreatedEvent;
 import com.bookero.common.BookeroProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -25,18 +26,17 @@ public class BookingCreatedEventListener {
     this.algorithmRunService = algorithmRunService;
   }
 
+  @Async("repriceExecutor")
   @TransactionalEventListener
   public void onBookingCreated(BookingCreatedEvent event) {
-    if (!properties.repriceAfterBooking()) {
+    if (!Boolean.TRUE.equals(properties.repriceAfterBooking())) {
       return;
     }
 
     try {
-      var algorithmKey = properties.repriceAfterBookingKey();
-      algorithmRunService.execute(algorithmKey, List.of(event.flightId()));
+      algorithmRunService.execute(properties.repriceAfterBookingKey(), List.of(event.flightId()));
     } catch (Exception e) {
-      log.warn("Post-booking reprice failed for flight {}: {}", event.flightId(), e.getMessage());
-      // Do not propagate; booking should not fail due to optional reprice
+      log.warn("post-booking reprice skipped for flight {}: {}", event.flightId(), e.getMessage());
     }
   }
 }
